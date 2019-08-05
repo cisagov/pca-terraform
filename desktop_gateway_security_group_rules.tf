87ea36344402243c0ec2dcf53d6a19f6f2f81d80
Dave Redmin <david.redmin@beta.dhs.gov>
2019-08-05 10:14:30 -0400

    Clean up access rules for Private ACL and Desktop Gateway security group

--- desktop_gateway_security_group_rules.tf
+++ desktop_gateway_security_group_rules.tf
@@ -22,42 +22,26 @@ resource "aws_security_group_rule" "desktop_gw_egress_to_gophish_via_ssh" {
   to_port           = 22
 }
 
-# # Allow guacamole web client ingress from the guacamole instance
-# # For: User access to guacamole web docker container
-# resource "aws_security_group_rule" "guacamole_client_ingress_from_desktop_gw" {
-#   security_group_id = "${aws_security_group.pca_desktop_gateway.id}"
-#   type              = "ingress"
-#   protocol          = "tcp"
-#   cidr_blocks = [
-#     "${aws_instance.guacamole.private_ip}/32"
-#   ]
-#   from_port = 8080
-#   to_port   = 8080
-# }
-#
-# # IS THIS NEEDED?
-# # Allow guacd ingress from the guacamole instance
-# resource "aws_security_group_rule" "guacd_ingress_from_desktop_gw" {
-#   security_group_id = "${aws_security_group.playground_private_sg.id}"
-#   type              = "ingress"
-#   protocol          = "tcp"
-#   cidr_blocks = [
-#     "${aws_instance.guacamole.private_ip}/32"
-#   ]
-#   from_port = 4822
-#   to_port   = 4822
-# }
-#
-# # Allow ingress via ephemeral ports from the gophish instance
-# resource "aws_security_group_rule" "desktop_gw_ingress_from_gophish_via_ephemeral_ports" {
-#   security_group_id        = aws_security_group.pca_desktop_gateway.id
-#   type                     = "ingress"
-#   protocol                 = "tcp"
-#   cidr_blocks              = ["${aws_instance.gophish.private_ip}/32"]
-#   from_port                = 1024
-#   to_port                  = 65535
-# }
+# Allow ingress from trusted networks via port 8443 (nginx/guacamole web)
+# For: PCA team access to guacamole web client
+# TODO: Modify this access when VPN solution is implemented
+resource "aws_security_group_rule" "desktop_gw_ingress_from_trusted_via_port_8443" {
+  security_group_id = aws_security_group.pca_desktop_gateway.id
+  type              = "ingress"
+  protocol          = "tcp"
+  cidr_blocks       = var.trusted_ingress_networks_ipv4
+  ipv6_cidr_blocks  = var.trusted_ingress_networks_ipv6
+  from_port         = 8443
+  to_port           = 8443
+}
 
-# TODO - ADD:
-# Egress to gophish instance for remote desktop protocols?
-# Ingress from gophish instance for remote desktop protocols?
+# Allow egress via VNC to the gophish instance
+# For: PCA team VNC access to gophish instance
+resource "aws_security_group_rule" "desktop_gw_egress_to_gophish_via_vnc" {
+  security_group_id = aws_security_group.pca_desktop_gateway.id
+  type              = "egress"
+  protocol          = "tcp"
+  cidr_blocks       = ["${aws_instance.gophish.private_ip}/32"]
+  from_port         = 5901
+  to_port           = 5901
+}
--- private_acl_rules.tf
+++ private_acl_rules.tf
@@ -1,5 +1,5 @@
 # Allow ingress from anywhere via ssh
-# Ingress is allowed from trusted networks via
+# Ingress is further restricted to only trusted networks via
 # the desktop gateway security group
 # For: DevOps ssh access to private subnet
 # TODO: Modify this access when VPN solution is implemented
@@ -14,13 +14,26 @@ resource "aws_network_acl_rule" "private_ingress_from_anywhere_via_ssh" {
   to_port        = 22
 }
 
+# Allow egress to anywhere via HTTPS
+# For: GoPhish fetches resources from https://fonts.googleapis.com
+resource "aws_network_acl_rule" "private_egress_to_anywhere_via_https" {
+  network_acl_id = aws_network_acl.pca_private.id
+  egress         = true
+  protocol       = "tcp"
+  rule_number    = "101"
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
+  from_port      = 443
+  to_port        = 443
+}
+
 # Allow egress to anywhere via ephemeral ports
 # For: DevOps ssh access to private subnet
 resource "aws_network_acl_rule" "private_egress_to_anywhere_via_ephemeral_ports" {
   network_acl_id = aws_network_acl.pca_private.id
   egress         = true
   protocol       = "tcp"
-  rule_number    = "101"
+  rule_number    = "102"
   rule_action    = "allow"
   cidr_block     = "0.0.0.0/0"
   from_port      = 1024
@@ -33,7 +46,7 @@ resource "aws_network_acl_rule" "private_egress_to_operations_via_ssh" {
   network_acl_id = aws_network_acl.pca_private.id
   egress         = true
   protocol       = "tcp"
-  rule_number    = "102"
+  rule_number    = "103"
   rule_action    = "allow"
   cidr_block     = aws_subnet.pca_operations.cidr_block
   from_port      = 22
@@ -41,18 +54,44 @@ resource "aws_network_acl_rule" "private_egress_to_operations_via_ssh" {
 }
 
 # Allow ingress from operations subnet via ephemeral ports
-# For: DevOps ssh access from private subnet to operations subnet
+# For: DevOps ssh access from private subnet to operations subnet and
+#      PCA team VNC access from private subnet to operations subnet
 resource "aws_network_acl_rule" "private_ingress_from_operations_via_ephemeral_ports" {
   network_acl_id = aws_network_acl.pca_private.id
   egress         = false
   protocol       = "tcp"
-  rule_number    = "103"
+  rule_number    = "104"
   rule_action    = "allow"
   cidr_block     = aws_subnet.pca_operations.cidr_block
   from_port      = 1024
   to_port        = 65535
 }
 
-# TODO - ADD:
-# Egress to operations subnet for remote desktop protocols?
-# Ingress from operations subnet for remote desktop protocols?
+# Allow ingress from anywhere via port 8443 (nginx/guacamole web)
+# Ingress is further restricted to only trusted networks via
+# the desktop gateway security group
+# For: PCA team access to guacamole web client
+# TODO: Modify this access when VPN solution is implemented
+resource "aws_network_acl_rule" "private_ingress_from_anywhere_via_port_8443" {
+  network_acl_id = aws_network_acl.pca_private.id
+  egress         = false
+  protocol       = "tcp"
+  rule_number    = "110"
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
+  from_port      = 8443
+  to_port        = 8443
+}
+
+# Allow egress to operations subnet via VNC
+# For: PCA team VNC access from private subnet to operations subnet
+resource "aws_network_acl_rule" "private_egress_to_operations_via_vnc" {
+  network_acl_id = aws_network_acl.pca_private.id
+  egress         = true
+  protocol       = "tcp"
+  rule_number    = "120"
+  rule_action    = "allow"
+  cidr_block     = aws_subnet.pca_operations.cidr_block
+  from_port      = 5901
+  to_port        = 5901
+}
